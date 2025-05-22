ebea0185137e30aaf1e60c49f66d8d40bc1ce68e
RWA82 <rwalter917@gmail.com>
2025-05-22 15:17:05 -0400

    Update device.mk

--- device.mk
+++ device.mk
@@ -15,8 +15,8 @@ AB_OTA_POSTINSTALL_CONFIG += \
 
 # Boot control HAL
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.2-impl \
-    android.hardware.boot@1.2-service
+    android.hardware.boot@1.1-impl \
+    android.hardware.boot@1.1-service
 
 PRODUCT_PACKAGES += \
     bootctrl.pineapple
